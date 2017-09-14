70cb1f80394b4de91ff65abd219ab7ac9eef4376
nile16 <nils.leandersson@gmail.com>
2017-09-14 22:11:18 +0200

    Added stuff

--- forecast.js
+++ forecast.js
@@ -3,13 +3,16 @@ var http = require('http');
 
 exports.get = function (callback) {
 
-    http.get({
-				hostname: 'opendata-download-metfcst.smhi.se',
-				path: '/api/category/pmp1.5g/version/2/geotype/point/lon/15.27195/lat/56.21253/data.json'
-			}, function(response) {
-				var body = '';
-				response.on('data', function(d) { body += d; });
-				response.on('end', function() { callback(JSON.parse(body).timeSeries); });
-    });
+	return new Promise((resolve, reject) => {
+
+		http.get({
+					hostname: 'opendata-download-metfcst.smhi.se',
+					path: '/api/category/pmp1.5g/version/2/geotype/point/lon/15.27195/lat/56.21253/data.json'
+				}, function(response) {
+					var body = '';
+					response.on('data', function(d) { body += d; });
+					response.on('end', function() { resolve(JSON.parse(body).timeSeries); });
+		});
+	})
 }
 
--- temp.js
+++ temp.js
@@ -2,15 +2,17 @@
 var http = require('http');
 
 exports.get = function (callback) {
+	
+	return new Promise((resolve, reject) => {
 
-    http.get({
-				hostname: 'nile16.nu',
-				port: 1251,
-				path: '/'
-			}, function(response) {
-				var body = '';
-				response.on('data', function(d) { body += d; });
-				response.on('end', function() { callback(JSON.parse(body)); });
-    });
+		http.get({
+					hostname: 'nile16.nu',
+					port: 1251,
+					path: '/'
+				}, function(response) {
+					var body = '';
+					response.on('data', function(d) { body += d; });
+					response.on('end', function() { resolve(JSON.parse(body)); });
+		});
+	})
 }
-
