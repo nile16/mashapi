b10654a37b40c3dfd0470820f591c634ffbf3513
nile16 <nils.leandersson@gmail.com>
2017-09-18 22:51:05 +0200

    Update mashapi.js

--- mashapi.js
+++ mashapi.js
@@ -6,21 +6,24 @@ const http        = require('http');
 
 
 setInterval( function(){
-	var data          = {};
-	var d = new Date();
-	data.time=d.toLocaleString();
-	forecast.get().then( (forecast) => {
-		data.forecast = forecast;
-	}).then( temp.get().then( (temp) => {
-		data.temp = temp;
-	})).then( () => {
+
+	var pf = forecast.get();
+	var pt = temp.get();
+	
+	Promise.all([pf,pt]).then( (x) => {
+		var data          = {};
+		var d = new Date();
+		data.time = d.toLocaleString();
+		data.forecast = x[0];
+		data.temp = x[1];
 		request({ method: 'GET', uri: 'http://192.168.1.200:5984/forecast/1'}, function (error, response, body) {
 			data._rev = JSON.parse(body)._rev;
 			request({ method: 'PUT', uri: 'http://192.168.1.200:5984/forecast/1', json: data }, function (error, response, body) {
 				if (error) console.log(error);
 			});
 		});
-	});
+	});;
+
 },1800000);
 
 
